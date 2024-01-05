1bb2ae82867005222fff2375beeb97c80546b40c
Cristiane ferreira <crislh2017@gmail.com>
2024-01-05 20:51:05 -0300

    concluido

---
+++ Nova pasta (2)/Exercicio 18/script.js
@@ -0,0 +1,13 @@
+// 18. Uma loja de eletrodomésticos estabeleceu as seguintes
+// modalidades de pagamento para a venda de suas mercadorias:
+
+// avista 2.5%
+// 2x até 5x preço de tabela sem desconto
+// 6x até 10x juros de 6% sobre o preço da tabela
+// 11x até 15x juros de 13% sobre o preço da tabela
+
+// Escreva um algoritmo que armazene o preço de tabela e o número
+// de vezes em que o pagamento será feito. Calcule o valor de cada
+// parcela e o preço total da compra e imprima no console.
+
+
